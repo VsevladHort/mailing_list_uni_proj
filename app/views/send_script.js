83f814ba686d134dc30631ce17970d502484f01a
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2023-03-15 15:47:38 +0200

    add default choices for email body

--- app/views/send_script.js
+++ app/views/send_script.js
@@ -1,4 +1,8 @@
 const documentForm = document.getElementById('document-form');
+const defaultMessages = ['I have been trying to reach you about your cars extended warranty',
+    'Your subscription is about to expire']
+const dropdown = document.getElementById('dropdown');
+dropdown.style.display = 'none';
 
 function sendEmail(data) {
     console.log(data);
@@ -35,4 +39,29 @@ documentForm.addEventListener('submit', function (event) {
         .catch(function (error) {
             alert('Error sending email: ' + error.message);
         });
+});
+
+documentForm.elements['text'].addEventListener('click', (_) => {
+    dropdown.innerHTML = '';
+    defaultMessages.forEach(function (text) {
+        const linkElement = document.createElement('p');
+        linkElement.textContent = text;
+        linkElement.classList.add("dropdown_option");
+        linkElement.addEventListener('click', function () {
+            documentForm.elements["text"].value = text;
+            dropdown.innerHTML = '';
+        });
+        dropdown.appendChild(linkElement);
+    });
+    dropdown.style.display = 'block';
+});
+
+documentForm.elements['text'].addEventListener('input', function (_) {
+    dropdown.style.display = 'none';
+});
+
+document.addEventListener('click', function (event) {
+    if (event.target !== documentForm.elements['text'] && event.target !== dropdown) {
+        dropdown.style.display = 'none';
+    }
 });
